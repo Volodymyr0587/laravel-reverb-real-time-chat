453c6df738dfd4d4e62af2c43dabdec38b7ec572
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-24 16:13:13 +0200

    Listen events; Show messages

--- resources/views/livewire/chatbox.blade.php
+++ resources/views/livewire/chatbox.blade.php
@@ -1,6 +1,8 @@
 <?php
 use App\Events\MessageSent;
+use Livewire\Attributes\On;
 use Livewire\Volt\Component;
+
 new class extends Component
 {
     /**
@@ -8,14 +10,18 @@ new class extends Component
      */
     public array $messages = [];
     public string $message = '';
+
     public function addMessage()
     {
         MessageSent::dispatch(auth()->user()->name, $this->message);
         $this->reset('message');
     }
+
+    #[On('echo:messages,MessageSent')]
     public function onMessageSent($event)
     {
-        dd($event);
+        // dd($event);
+        $this->messages[] = $event;
     }
 }
 ?>
@@ -34,7 +40,7 @@ new class extends Component
         <div class="w-full h-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded overflow-auto flex flex-col px-2 py-4">
             <div x-ref="chatBox" class="flex-1 p-4 text-sm flex flex-col gap-y-1">
                 @foreach($messages as $message)
-                    <div><span class="text-indigo-600">{{ $message['name'] }}:</span> <span class="dark:text-white">{{ $message['message'] }}</span></div>
+                    <div><span class="text-indigo-600">{{ $message['name'] }}:</span> <span class="dark:text-white">{{ $message['text'] }}</span></div>
                 @endforeach
             </div>
             <div>
